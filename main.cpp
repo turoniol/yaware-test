739f397218a11b7c6e02c30fe61f3ced0cbd64cf
turoniol <turoniol29@gmail.com>
2023-07-13 18:25:18 +0300

    Added basic functional for screenshot making, saving and loading using SQLite.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <QSqlDatabase>
+#include <QSqlQuery>
+
+#include <QScreen>
+#include <QPixmap>
+#include <QBuffer>
+#include <QByteArray>
+#include <QDateTime>
+#include <QSqlError>
+#include <QSqlRecord>
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
@@ -13,6 +24,61 @@ int main(int argc, char *argv[])
                 QCoreApplication::exit(-1);
         }, Qt::QueuedConnection);
     engine.load(url);
+
+    QSqlDatabase m_db = QSqlDatabase::addDatabase("QSQLITE");
+    m_db.setDatabaseName("./images.db");
+
+    if (!m_db.open())
+        qDebug() << "Error: connection with database failed";
+    else
+        qDebug() << "Database: connection ok";
+
+
+    assert(QSqlQuery("create table if not exists screenshots ("
+                     "   id int primary key, "
+                     "   image_data blob not null, "
+                     "   image_hash text not null, "
+                     "   comparison_percent real not null, "
+                     "   date text not null);")
+               .exec());
+
+    if (QScreen *screen = QGuiApplication::primaryScreen()) {
+        auto pixmap = screen->grabWindow(0);
+
+        QByteArray bytes;
+        QBuffer buffer(&bytes);
+        buffer.open(QIODevice::WriteOnly);
+        pixmap.save(&buffer, "PNG");
+
+        QSqlQuery q;
+        q.prepare("insert into screenshots (image_data, image_hash, comparison_percent, date) "
+                  "values (:image_data, 'imagehash', 0, :date);");
+        q.bindValue(":image_data", bytes);
+        QString currentTime = QDateTime::currentDateTime().toString(Qt::ISODate);
+        q.bindValue(":date", currentTime);
+
+
+        if (q.exec())
+            qDebug() << "Insert: ok";
+        else
+            qDebug() << "Error while inserting" << q.lastError().text();
+    }
+
+    QSqlQuery q("select * from screenshots;");
+    auto record = q.record();
+
+    int dataCol = record.indexOf("image_data");
+
+    while (q.next()) {
+        QByteArray dataArr = q.value(dataCol).toByteArray();
+
+        QPixmap pixmap;
+        if (pixmap.loadFromData(dataArr)) {
+            pixmap.save(filename);
+        }
+    }
+
+    m_db.close();
 
     return app.exec();
 }
--- main.qml
+++ main.qml
@@ -1,6 +1,7 @@
 import QtQuick 2.15
 import QtQuick.Window 2.15
 import QtQuick.Controls 2.15
+import QtQuick.Layouts 1.0
 
 ApplicationWindow {
     width: 1280
@@ -23,14 +24,33 @@ ApplicationWindow {
         }
     }
 
-    footer: Button {
-        property bool isActive: false
+    footer: ColumnLayout {
+        Rectangle {
+            color: "yellow"
+            anchors.fill: parent
+        }
 
-        text: "Start"
+        RowLayout {
+            Text {
+                text: "Delay (s)"
+            }
+            SpinBox {
+                from: 0; to: 60
+                value: 60
+                stepSize: 5
+            }
+        }
+        Button {
+            property bool isActive: false
 
-        onPressed: {
-            isActive = !isActive
-            text = isActive ? "Stop" : "Start"
+            text: "Start"
+
+            width: parent.width
+
+            onPressed: {
+                isActive = !isActive
+                text = isActive ? "Stop" : "Start"
+            }
         }
     }
 }
